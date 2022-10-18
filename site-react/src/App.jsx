36d2393dc4fe15fabdd317ca6a5e52f2ef420f8f
Mecanight <cassianoprediger@gmail.com>
2022-10-18 00:09:23 -0300

    Correcao erros aula 13-10-22

--- site-react/src/App.jsx
+++ site-react/src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
-import { Routes } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import { HomePage } from './pages/Home'
 
 function App() {
 
